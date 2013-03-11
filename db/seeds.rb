07a0ace1e6006fa1d75b902c74a987be0cee40ef
Blanchet <h55nick@gmail.com>
2013-03-11 15:59:09 -0400

    fixed logout, database git

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.delete_all
 
-User.create(:name => "admin",:email=>nil,:password=>'abc123', :password_confirmation=>'abc123', :is_admin=>true)
-User.create(:name => "user",:email=>nil,:password=>'abc123', :password_confirmation=>'abc123', :is_admin=>false)
-User.create(:name => "Nick",:email=>"h55nick@gmail.com",:password=>'aa', :password_confirmation=>'abc123', :is_admin=>true)
+User.create(:name => "admin",:email=>"admin@gmail.com",:password=>'abc123', :password_confirmation=>'abc123', :is_admin=>true)
+User.create(:name => "user",:email=>"user@gmail.com",:password=>'abc123', :password_confirmation=>'abc123', :is_admin=>false)
+User.create(:name => "Nick",:email=>"h55nick@gmail.com",:password=>'aa', :password_confirmation=>'aa', :is_admin=>true)

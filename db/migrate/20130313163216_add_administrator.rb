8fce54c6291e2ae925a49d270bdebff203ac7eb1
Blanchet <h55nick@gmail.com>
2013-03-14 11:45:43 -0400

    migration order

--- db/migrate/20130313163216_add_administrator.rb
+++ db/migrate/20130313163216_add_administrator.rb
@@ -1,6 +1,5 @@
 class AddAdministrator < ActiveRecord::Migration
   def change
-    remove_columns :users, :subscriber_id
     add_column :users, :userable_id, :integer
     add_column :users, :userable_type, :string
   end
